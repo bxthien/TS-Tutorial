4f5838c6f454b0f68df4aa8c0376e5741609b803
bxthien <buixuanthien.0105@gmail.com>
2024-07-18 16:24:38 +0700

    update todo list - Single todo

--- src/App.tsx
+++ src/App.tsx
@@ -67,20 +67,21 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
-
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if(todo) { 
-      setTodos([...todos, {
-        id: Date.now(),
-        todo: todo,
-        isDone: false
-      }])
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          todo: todo,
+          isDone: false,
+        },
+      ]);
 
-      setTodo("")
+      setTodo('');
     }
-
   };
 
   console.log(todos);
@@ -88,8 +89,8 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/> 
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import { MdEditSquare, MdDeleteForever, MdOutlineFileDownloadDone } from "react-icons/md";
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  MdEditSquare,
+  MdDeleteForever,
+  MdOutlineFileDownloadDone,
+} from 'react-icons/md';
 
 interface Props {
   todo: Todo;
   todos: Todo[];
-  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-  
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter(toto => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map(todo => (todo.id === id ? { ...todo, todo: editTodo } : todo)),
+    );
+    setEdit(false);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='todos__single'>
-      <span className="todo__single--text">
-        {todo. todo}
-      </span>
-      <div>
-        <span className="icon"><MdEditSquare /></span>
-        <span className="icon"><MdDeleteForever /></span>
-        <span className="icon"><MdOutlineFileDownloadDone /></span>
+    <form className="todos__single" onSubmit={e => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          className="todos__single--text"
+          value={editTodo}
+          onChange={e => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
+        <s className="todo__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todo__single--text">{todo.todo}</span>
+      )}
+
+      <div className="icon__style">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <MdEditSquare />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <MdDeleteForever />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdOutlineFileDownloadDone />
+        </span>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
